354e519b1a41a143339896aef87c704e33b97cef
Vipipi <konamiv3@hotmail.com>
2021-10-13 21:06:56 -0700

    Update LC1048.py
    add DP method

--- LC1048.py
+++ LC1048.py
@@ -26,3 +26,24 @@ class Solution:
             bt(word, 1)
             
         return res
+    
+"""Dynamic Programming"""
+class Solution:
+    def longestStrChain(self, words: List[str]) -> int:
+        if len(words) == 1:
+            return 1
+        dp = collections.defaultdict(int)
+        res = 0
+        words.sort(key = lambda x: len(x))
+        for word in words:
+            if len(word) == 1:
+                dp[word] = 1
+            else:
+                local_max = 0
+                for i in range(len(word)):
+                    key = word[:i] + word[i+1:]
+                    local_max = max(local_max, dp[key])
+                dp[word] = local_max + 1
+                res = max(res, dp[word])
+                        
+        return res
